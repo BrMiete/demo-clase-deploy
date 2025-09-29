271739226cb259a5cb28a0445f6a466c0a7cb7cc
BrMiete <braian.miete@gmail.com>
2025-09-29 19:46:35 -0300

    GH Pages changes

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  server: {
+  /*server: {
     port: 3000,
     open: true
   },
   build: {
     outDir: 'dist',
     sourcemap: true
-  },
-  base: '/demo-clase-deploy/'
+  },*/
+  plugins: [react()],
+  base: '/demo-clase-deploy'
 });
